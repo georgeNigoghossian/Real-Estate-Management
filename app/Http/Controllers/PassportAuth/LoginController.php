bee25af6a98be1ad5806e09497ac420d0e934a59
GeoN <geon200181@gmail.com>
2023-07-03 21:27:23 +0300

    Overriding login method from AuthencticateUsers to LoginController

--- app/Http/Controllers/PassportAuth/LoginController.php
+++ app/Http/Controllers/PassportAuth/LoginController.php
@@ -6,12 +6,67 @@ use App\Http\Controllers\App\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Laravel\Passport\RefreshTokenRepository;
 use Laravel\Passport\TokenRepository;
 
 class LoginController extends Controller
 {
     use AuthenticatesUsers;
+
+    public function login(Request $request)
+    {
+
+
+        $this->validateLogin($request);
+
+        // If the class is using the ThrottlesLogins trait, we can automatically throttle
+        // the login attempts for this application. We'll key this by the username and
+        // the IP address of the client making these requests into this application.
+        if (method_exists($this, 'hasTooManyLoginAttempts') &&
+            $this->hasTooManyLoginAttempts($request)) {
+            $this->fireLockoutEvent($request);
+
+            return $this->sendLockoutResponse($request);
+        }
+
+        if ($this->attemptLogin($request)) {
+            if ($request->hasSession()) {
+                $request->session()->put('auth.password_confirmed_at', time());
+            }
+
+            return $this->sendLoginResponse($request);
+        }
+
+        // If the login attempt was unsuccessful we will increment the number of attempts
+        // to login and redirect the user back to the login form. Of course, when this
+        // user surpasses their maximum number of attempts they will get locked out.
+        $this->incrementLoginAttempts($request);
+        return $this->sendFailedLoginResponse($request);
+    }
+
+    protected function sendLoginResponse(Request $request)
+    {
+        $request->session()->regenerate();
+
+        $this->clearLoginAttempts($request);
+
+
+        if ($response = $this->authenticated($request, $this->guard()->user())) {
+            return $response;
+        }
+
+        $accessToken = Auth::user()->createToken('authToken')->accessToken;
+
+        $response=[
+            'user' => Auth::user(),
+            'access_token' => $accessToken,
+        ];
+        return $request->wantsJson()
+            ? new JsonResponse($response, 200)
+            : redirect()->intended($this->redirectPath());
+
+    }
 
     public function logout(Request $request)
     {
