adcdb9535c08fc78191bd8183438d5f2e61b5b33
ritarezek200185 <ritarezek200185@gmail.com>
2023-07-15 12:25:47 +0300

    added user seeder

---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Rita',
+            'email' => 'rita@gmail.com',
+            'password' => Hash::make('password'),
+            'sms_verified_at' => '2023-07-15 11:52:50',
+            'mobile' => '0932909535'
+        ]);
+
+    }
+}
