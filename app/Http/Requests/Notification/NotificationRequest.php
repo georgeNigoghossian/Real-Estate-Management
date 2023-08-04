075c77161f5cf6f2ad164d696598b6961bf2e9e1
ritarezek200185 <ritarezek200185@gmail.com>
2023-08-04 23:29:06 +0300

    complete sendNotification api

---
+++ app/Http/Requests/Notification/NotificationRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests\Notification;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class NotificationRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [
+          'title'=>['string'],
+          'body'=>['string'],
+          'image'=>['string'],
+        ];
+    }
+
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+}
