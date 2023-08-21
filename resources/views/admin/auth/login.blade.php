31cb6b4cf826dbb90536a6fb8244837a17f5562c
nicolas_alahmar <nicolasalahmar@gmail.com>
2023-08-21 22:30:11 +0300

    .

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -15,7 +15,7 @@
                         <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
                             <div class="bg-gradient-primary shadow-primary border-radius-lg py-3 pe-1">
                                 <h4 class="text-white font-weight-bolder text-center mt-2 mb-0">
-                                    Welcome to <b>Nosa Casa</b> Admin Panel
+                                    Welcome to <b>NostraCasa</b> Admin Panel
 
                                 </h4>
                                 <br>
